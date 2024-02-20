c1ff06e31f33ae07a6fa5cd54e67e4f6a05a0b5e
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-02-20 15:13:47 +0200

    Completed Constructor Creation

---
+++ PS_45_Veselin/Welcome/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Others;
+
+namespace Welcome.Model
+{
+    internal class User
+    {
+        public string Names;
+        public string Password;
+        public UserRolesEnum Role;
+
+        public User(String Names, String Password, UserRolesEnum Role) {
+            this.Names = Names;
+            this.Password = Password;
+            this.Role = Role;
+        }   
+
+    }
+}
