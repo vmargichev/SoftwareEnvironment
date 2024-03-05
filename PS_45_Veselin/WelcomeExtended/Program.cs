e11024e0e0d51b24c56bfdd3acf3da2917bcafe6
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-03-05 15:17:59 +0200

    Completed 2nd Task

---
+++ PS_45_Veselin/WelcomeExtended/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using Welcome.Model;
+using Welcome.Others;
+using Welcome.ViewModel;
+using WelcomeExtended.Others;
+
+namespace WelcomeExtended
+{
+    internal class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                var user = new User
+                (
+                    "John Smith",
+                    "password123",
+                    UserRolesEnum.STUDENT
+                );
+
+                var viewModel = new UserViewModel(user);
+
+                var view = new UserView(viewModel);
+
+                view.Display();
+
+                view.DisplayError();
+            }
+            catch (Exception e)
+            {
+                var log = new ActionOnError(Delegates.Log);
+                log(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executed in any case!");
+            }
+            }
+        }
+    }
+
